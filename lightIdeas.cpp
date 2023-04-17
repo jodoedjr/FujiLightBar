aa1ea1d8113aae1da2baf0be612f60f79d8c4671
John Edwards <jdedwards6789@gmail.com>
2023-04-17 11:09:25 -0400

    early commit

---
+++ lightIdeas.cpp
@@ -0,0 +1,16 @@
+//staggered slant - variable speeds, directions, density
+/*
+O X O O O X O O O X O O O
+O O X O O O X O O O X O O
+
+O O X O O O X O O O X O O
+O O O X O O O X O O O X O
+
+O X O O X O O X O O X O O
+O O X O O X O O X O O X O
+
+X O O X O O X O O X O O X
+O X O O X O O X O O X O O
+*/
+
+//EARTH QUAKE - any other mode, but EARTH QUAKE flashes concerningly
